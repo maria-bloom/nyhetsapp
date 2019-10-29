9931f705d018f1c5cd18e2827b737cd517bb6475
Maria <Maria@Marias-MacBook-Pro.local>
2019-10-29 19:51:01 +0100

    Lagt till egna komponenter

---
+++ src/Components/Nyhetsartikel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+funktion Nyhetsartikel() {
+    return (
+        <articles>
+            <img src="https://source.unsplash.com/random/200x125/?rabbit" />
+            <h2>Testnyhet</h2>
+            <p>Beskrivning av testnyheten</p>
+            <a>Läs mer..</a>
+        </articles>
+    );
+ }
+
+export default Nyhetsartikel;
---
+++ src/Components/Nyhetslista.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nyhetsartikel from '.Nyhetsartikel';
+funktion Nyhetslista() {
+    return <section>
+        <Nyhetsartikel />
+        <Nyhetsartikel />
+        <Nyhetsartikel />
+        <Nyhetsartikel />
+       
+</section>);
+}
+
+export default Nyhetslista;
